a922be1afbf98f567243b4dc1b6629429afe6502
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 11:24:59 +0530

    Read Single Digit Number

--- SelectionPracticeProblem.js
+++ SelectionPracticeProblem.js
@@ -1,4 +1,42 @@
 console.log("Welcome to Selection Practice Problem")
 
+//Selection Practice Problems with if else if and else
 
+//Printing Digit 
+let number = Math.floor(Math.random()*10)%10;
+console.log("Number : "+ number);
 
+if (number == 1) 
+{
+    console.log("One");
+} else if (number == 2) 
+{
+    console.log("Two");
+} else if (number == 3) 
+{
+    console.log("Three");
+} else if (number == 4) 
+{
+    console.log("Four");
+} else if (number == 5) 
+{
+    console.log("Five");
+} else if (number == 6) 
+{
+    console.log("Six");
+} else if (number == 7) 
+{
+    console.log("Seven");
+} else if (number == 8)
+{
+    console.log("Eight");
+} else if (number == 9)
+{
+    console.log("Nine");
+} else if (number == 0) 
+{
+    console.log("Zero");
+} else 
+{
+    console.log("Invalid number");
+}
